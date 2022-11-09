3325dc6ef1f404dd91d8db8954408e3c02aa71e1
mammucodes <mammucodes@gmail.com>
2022-11-09 12:23:40 +0530

    commited program on 9/11

--- src/com/mamatha/linkedlist/Ex15to20.java
+++ src/com/mamatha/linkedlist/Ex15to20.java
@@ -11,10 +11,12 @@ public class Ex15to20 {
 	lsdata.add("blue");
 	lsdata.add("orange");
 	lsdata.add("purple");
+	lsdata.add("black");
+	lsdata.add("green");
 	System.out.println(lsdata);
 	//Collections.swap(lsdata, 0, 2);
 	
-	//now without using swapping
+	//now without using collection  swapping
 	
 		String temp =  lsdata.get(0);
 	String  ls = 	lsdata .set(0, lsdata.get(2));
@@ -24,6 +26,17 @@ public class Ex15to20 {
 		
 		System.out.println("after swapping LL");
 		System.out.println(lsdata);
+		//ex 16 th code
+		Collections.shuffle(lsdata);
+		System.out.println("after shuffling ");
+		System.out.println(lsdata);
+		//Ex 17
+		LinkedList l2 = new LinkedList<>();
+		l2.add("Red Roses");
+		l2.add("yellow sunflower");
+		l2.add("green leafs");
+		
+	
 	
 
 	}
